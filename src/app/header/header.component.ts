6e31ba124d0f86ae84a1381c9df7804c6a8dc0af
SheryaarButt <66394963+SheryaarButt@users.noreply.github.com>
2020-10-29 14:51:53 -1000

    -Finished edit links -Cleaned up unused code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,20 +10,7 @@ export class HeaderComponent implements OnInit {
   constructor() {
   }
 
-  public static DISPLAY_CAT_RECIPE = 'recipe';
-  public static DISPLAY_CAT_SHOPPING = 'shopping';
-
-  @Output() displayCategoryToggled = new EventEmitter<string>();
-
   ngOnInit(): void {
-  }
-
-  displayRecipe(): void {
-    this.displayCategoryToggled.emit(HeaderComponent.DISPLAY_CAT_RECIPE);
-  }
-
-  displayShopping(): void {
-    this.displayCategoryToggled.emit(HeaderComponent.DISPLAY_CAT_SHOPPING);
   }
 
 }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,7 +13,7 @@ export class RecipeDetailComponent implements OnInit {
   selectedRecipe: Recipe;
 
   constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -24,6 +24,9 @@ export class RecipeDetailComponent implements OnInit {
   }
   ingredientsToShoppingList(): void {
     this.recipeService.ingredientsToShoppingList(this.selectedRecipe.ingredients);
+  }
+  toEditRecipe(): void {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
